1f3781a0a5b31bed682e3c59043652730214948e
Fabio Canaza <fbcanaza@gmail.com>
2025-04-03 19:22:07 +0200

    conflictos al actualizar

--- PracticasM3-Programacio/AC11/obj/Debug/net8.0/AC11.AssemblyInfo.cs
+++ PracticasM3-Programacio/AC11/obj/Debug/net8.0/AC11.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AC11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab8e8deb429d45c252a40868c3f848b7a9a263a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d38d8fc46110457263b5185594091f1adfb2fc25")]
 [assembly: System.Reflection.AssemblyProductAttribute("AC11")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AC11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
